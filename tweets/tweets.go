dab1161142ce8a1631eac3ab7d489a6dc1e03f66
GuiBret <guillaume.bretzner@gmail.com>
2021-10-23 13:16:09 +0200

    Handle "Create tweet" endpoint

--- tweets/tweets.go
+++ tweets/tweets.go
@@ -1,15 +1,66 @@
 package tweets
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"twitter-ripoff/dbmgmt"
+	"twitter-ripoff/models"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 
 }
 
 func GetTweet(w http.ResponseWriter, r *http.Request) {
+
 }
 
-
 func WriteTweet(w http.ResponseWriter, r *http.Request) {
-	
+	var user models.User
+	var payload models.TweetPost
+	tweet := models.Tweet{}
+
+	user_id, err := strconv.Atoi(mux.Vars(r)["id"])
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "parsing error")
+		return
+	}
+
+	db, err := dbmgmt.GetDBInstance()
+	err = db.First(&user, user_id).Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "User not found")
+		return
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&payload)
+
+	if err != nil || payload.Message == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Invalid payload")
+		return
+	}
+
+	tweet.Content = payload.Message
+	tweet.FromID = int(user.ID)
+	err = db.Save(&tweet).Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Print(err)
+		return
+	}
+
+	res, err := json.Marshal(&tweet)
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, string(res))
 }
--- tweets/tweets_test.go
+++ tweets/tweets_test.go
@@ -1,13 +1,107 @@
 package tweets
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"twitter-ripoff/models"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestShouldReturn404SinceTweetDoesNotExist(t *testing.T) {
+type InvalidTweetPost struct {
+	Msg string `json:"msg"`
+}
+
+func TestShouldReturn404SinceUserDoesNotExist(t *testing.T) {
 	w := httptest.NewRecorder()
 
-	r, err := http.NewRequest(http.MethodGet, "http://localhost:4000/users/1/")
+	tweet, err := json.Marshal(models.TweetPost{"This is my message"})
+
+	if err != nil {
+		t.Fatal("Error")
+	}
+	r, err := http.NewRequest(http.MethodPost, "http://localhost:4000/users/100000/tweet", bytes.NewReader(tweet))
+
+	vars := map[string]string{
+		"id": "400000",
+	}
+	r = mux.SetURLVars(r, vars)
+
+	WriteTweet(w, r)
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(w.Result().Body)
+
+	assert.Equal(t, "User not found", buffer.String())
+	assert.Equal(t, http.StatusNotFound, w.Result().StatusCode, "Should return 404")
+
 }
+
+func TestShouldReturn400SinceInvalidPayload(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	tweet, err := json.Marshal(InvalidTweetPost{"This is my message"})
+
+	if err != nil {
+		t.Fatal("Error")
+	}
+	r, err := http.NewRequest(http.MethodPost, "http://localhost:4000/users/100000/tweet", bytes.NewReader(tweet))
+
+	vars := map[string]string{
+		"id": "1",
+	}
+	r = mux.SetURLVars(r, vars)
+
+	WriteTweet(w, r)
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(w.Result().Body)
+
+	assert.Equal(t, "Invalid payload", buffer.String())
+	assert.Equal(t, http.StatusBadRequest, w.Result().StatusCode, "Should return 400")
+
+}
+
+func TestShouldCreateTweet(t *testing.T) {
+
+	w := httptest.NewRecorder()
+
+	tweet, err := json.Marshal(models.TweetPost{"This is my message"})
+
+	if err != nil {
+		t.Fatal("Error")
+	}
+	r, err := http.NewRequest(http.MethodPost, "http://localhost:4000/users/100000/tweet", bytes.NewReader(tweet))
+
+	vars := map[string]string{
+		"id": "1",
+	}
+	r = mux.SetURLVars(r, vars)
+
+	WriteTweet(w, r)
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(w.Result().Body)
+	tweet_response := buffer.Bytes()
+
+	var tweet_obj models.TweetGet
+	json.Unmarshal(tweet_response, &tweet_obj)
+
+	assert.Equal(t, http.StatusCreated, w.Result().StatusCode, "Should return 201")
+	assert.Equal(t, "This is my message", tweet_obj.Message)
+
+}
+
+// func TestShouldReturn400SincePayloadInvalid(t *testing.T) {
+// 	w := httptest.NewRecorder()
+// }
+
+// func TestShouldReturn404SinceTweetDoesNotExist(t *testing.T) {
+// 	w := httptest.NewRecorder()
+
+// 	r, err := http.NewRequest(http.MethodGet, "http://localhost:4000/users/1/tweet/2")
+// }
